b66360d87674ba7bf20185e5091d9bf3712b95aa
TonyBaston <tony.baston@gmail.com>
2022-07-04 09:59:43 -0500

    first commit

---
+++ OdometryTB/src/main/java/frc/robot/subsystems/RomiDrivetrain.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class RomiDrivetrain extends SubsystemBase {
+  private static final double kCountsPerRevolution = 1440.0;
+  private static final double kWheelDiameterInch = 2.75591; // 70 mm
+
+  // The Romi has the left and right motors set to
+  // PWM channels 0 and 1 respectively
+  private final Spark leftMotor = new Spark(0);
+  private final Spark rightMotor = new Spark(1);
+  private final DifferentialDrive diffDrive = new DifferentialDrive(leftMotor, rightMotor);
+
+  /** Creates a new RomiDrivetrain. */
+  public RomiDrivetrain() {
+    rightMotor.setInverted(true);
+  }
+
+  public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
